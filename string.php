509e20ca3eea5ee5c84b1d0210e30dba09e662d8
Shariful Islam <sharifrony725@gmail.com>
2023-12-10 11:10:31 +0600

    recape php fundamentals

---
+++ string.php
@@ -0,0 +1,15 @@
+<?php
+
+    // $name = "sharifrony725";
+    // echo strlen($name);
+
+    // $des = "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Natus, nobis?";
+    // echo str_word_count($des);
+
+    // $word = 'Hello';
+    // echo strrev($word);
+
+    $userName = 'Sharif';
+    $userName2 = 'Rony';
+
+?>
